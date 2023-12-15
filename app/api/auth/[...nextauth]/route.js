01c6ccf810262d80c5c3ad8458da2d72864fe432
SHIJU G <85386417+shiju-g@users.noreply.github.com>
2023-12-15 14:16:09 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,16 +1,11 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import LinkedInProvider from "next-auth/providers/linkedin";
 
 export const authOptions = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-    LinkedInProvider({
-      clientId: process.env.LINKEDIN_CLIENT,
-      clientSecret: process.env.LINKEDIN_SECRET_ID,
     }),
   ],
   secret: process.env.NEXT_PUBLIC_SECRET, // Required secret key. Change this to something secret.
