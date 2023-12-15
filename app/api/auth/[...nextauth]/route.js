6708d4fe7ea534ff15448d6da054d0d95cab2805
SHIJU G <85386417+shiju-g@users.noreply.github.com>
2023-12-15 14:07:34 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -31,11 +31,6 @@ export const authOptions = NextAuth({
       }
       return token;
     },
-    async redirect(url, baseUrl) {
-      console.log("Redirect URL:", url);
-      console.log("Base URL:", baseUrl);
-      return url;
-    },
   },
 });
 
